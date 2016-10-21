e411ee738dc5dade713db27a323704a62f190b46
Swaumya <pokharelswaumya@gmail.com>
2016-10-21 11:39:26 +0545

    Add files via upload

---
+++ largest.c
@@ -0,0 +1,137 @@
+/*
+Student Name: Swaumya Pokharel
+Subject     :Programming Fundamental
+Roll        :
+Lab no      :
+Program     : Write a c program to print largest no using nested loop
+Date        :21st October 2016
+*/
+
+		
+		#include<stdio.h>
+		
+			int main()
+			{
+					int a,b,c;
+					printf("Enter the first number:\n");
+					scanf("%d",&a);
+
+					printf("Enter the second number:\n");
+					scanf("%d",&b);
+					
+					printf("Enter the third number:\n");
+					scanf("%d",&c);
+					
+					if(a>=b && a>=c)
+					printf("a is greatest\n");
+					
+					else if(b>=c && b>=a)
+					printf("b is greatest\n");
+					
+					else
+					printf("c is greatest");
+					 
+					return 0;
+			}
+			
+			
+			
+			
+			
+			
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			
+			
+			
+					
+					
