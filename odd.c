156d0c1af65ca4ef4704cfbefb32cef63f7a478d
Swaumya <pokharelswaumya@gmail.com>
2016-10-21 11:38:59 +0545

    Add files via upload

---
+++ odd.c
@@ -0,0 +1,131 @@
+/*
+Student Name: Swaumya Pokharel
+Subject     :Programming Fundamental
+Roll        :
+Lab no      :
+Program     : Write a c program to print odd and even number
+Date        :21st October 2016
+*/
+
+		
+		#include<stdio.h>
+		
+			int main()
+			{
+					int a;
+					printf("Enter the first number:\n");
+					scanf("%d",&a);
+
+					if(a%2==0)
+					 {
+						  printf("The number is even");
+						  }
+					 else
+					 {
+					 printf("The number is odd");
+					 }
+					 
+					return 0;
+			}
+			
+			
+			
+			
+			
+			
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			
+			
+			
+					
+					
