ed8d61a15e97e72d120c46334081277b3fd1164f
Swaumya <pokharelswaumya@gmail.com>
2016-10-21 11:41:05 +0545

    Add files via upload

---
+++ relation.c
@@ -0,0 +1,138 @@
+/*
+Student Name: Swaumya Pokharel
+Subject     :Programming Fundamental
+Roll        :
+Lab no      :
+Program     : Write a c program to use of assignment of operator
+Date        :20th October 2016
+*/
+
+		
+		#include<stdio.h>
+		
+			int main()
+			{
+					int a,b;
+					printf("Enter the first number:\n");
+					scanf("%d",&a);
+					printf("Enter the second number:\n");
+					scanf("%d",&b);
+					printf("The use of ==:\n");
+					if (a==b)
+					printf("the numbers are equal:\n");
+					else if (a>b)
+					printf("a is greatest\n");
+					else if (a<b)
+					printf("b is greatest\n");
+					else if (a!=b)
+					printf("the numbers are not equal\n");
+					else if (a>=b)
+					printf("a is greater or  less than to\n");
+					else if (a<b)
+					printf("b is greater or less than to\n");
+					else
+					printf("invalid");
+					return 0;
+					}
+			
+			
+			
+			
+			
+			
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			
+			
+			
+					
+					
