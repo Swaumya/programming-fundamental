acbc7c47f6f50de2da3ee730e0f8000242d22889
Swaumya <pokharelswaumya@gmail.com>
2016-10-24 10:53:27 +0545

    Add files via upload

---
+++ even.c
@@ -0,0 +1,24 @@
+/*
+Student Name: Swaumya Pokharel
+Subject     :Programming Fundamental
+Roll        :
+Lab no      :
+Program     : Write a c program to print oddd and even
+Date        :24th October 2016
+*/
+
+
+
+   #include<stdio.h>]
+   
+      int main()
+      {
+      
+       int a;
+       printf("Enter any number:\n");
+       scanf("%d",&a);
+       (a%2)==0?printf(" The number is Even=%d",a): printf(" The number is Odd=%d",a);
+       
+       
+       return 0;
+       }
