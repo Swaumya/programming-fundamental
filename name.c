fe1e00a954869e0ef65c6ece630e8237e1870786
Swaumya <pokharelswaumya@gmail.com>
2016-11-15 10:10:02 +0545

    Add files via upload

---
+++ name.c
@@ -0,0 +1,22 @@
+/*
+Student Name: Swaumya Pokharel
+Subject     :Programming Fundamental
+Roll        :
+Lab no      :
+Program     : Write a c program to print your name 10 times
+Date        :24th October 2016
+*/
+      
+      
+      #include<stdio.h>
+        
+        int main()
+        {
+        
+  
+			for(int i=1;i<=10;i++)
+			{
+			printf("\nSwaumya");	
+			 }
+			 return 0;
+			 }
