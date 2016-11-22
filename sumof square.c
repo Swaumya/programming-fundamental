d8a3c3834f920c187d7529c34cf06922e2072850
Swaumya <pokharelswaumya@gmail.com>
2016-11-22 10:35:14 +0545

    Add files via upload

---
+++ sumof square.c
@@ -0,0 +1,32 @@
+/*
+Student Name : Swaumya Pokharel
+Roll No      : 25
+Subject      : Programming Fundamental
+Program      : Write a program to find th sum of square of n natural numbers
+Lab no       : 16
+Date         : 22nd November 2016
+*/
+
+		#include<stdio.h>
+			
+				int main()
+				{
+				
+					int i,n,sum=0;
+					
+						printf("Enter any number\n");
+						scanf("%d",&n);
+						
+						 for(i=1;i<=n;i++)
+						{
+						  sum +=i*i;
+						  
+						  }
+						  
+							printf("\n The sum ofsquare natural numbers =%d",sum);
+							
+					return 0;
+				}
+				6		 
+2                                                                                                                                                                                                                                  
+
