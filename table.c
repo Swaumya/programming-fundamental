febbd179813600afe998ca3279e304ebe83b425e
Swaumya <pokharelswaumya@gmail.com>
2017-01-12 11:09:21 +0545

    Add files via upload

---
+++ table.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int main()
+ {
+ int i,j,mul;
+
+  for(i=1;i<=5;i++)
+  {
+    for(j=1;j<=10;j++)
+    {
+        mul=i*j;
+
+
+  printf("%d ",mul);
+  }
+    printf("\n");
+  }
+  return 0;
+  }
