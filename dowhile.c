585ab4c65e2308219b9709e6d6501ab01d5052e3
Swaumya <pokharelswaumya@gmail.com>
2017-01-12 11:07:33 +0545

    Add files via upload

---
+++ dowhile.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+  int main()
+    {
+      int i=2;
+
+      do
+      {
+      printf("%d\t",i);
+      i=i+2;
+      }
+      while(i<=100);
+      return 0;
+    }
