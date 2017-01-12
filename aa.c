70195a8908717a3ed2bfc79c7423735396b41bac
Swaumya <pokharelswaumya@gmail.com>
2017-01-12 11:06:23 +0545

    Add files via upload

---
+++ aa.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+  #include<conio.h>
+   int main ()
+   {
+    char ch='a';
+    int i=1;
+    while(i<=26)
+    {
+    i++;
+     printf("%c\t",ch);
+     ch++;
+         }
+         return 0;
+   }
