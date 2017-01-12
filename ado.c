a7ccda7a77a137f218493aaed52d7022bf8f4070
Swaumya <pokharelswaumya@gmail.com>
2017-01-12 11:06:42 +0545

    Add files via upload

---
+++ ado.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+  #include<conio.h>
+   int main ()
+   {
+    char ch='a';
+    int i=1;
+
+ do
+
+    {
+    i++;
+     printf("%c\t",ch);
+     ch++;
+         }
+         while(i<=26);
+         return 0;
+   }
+
