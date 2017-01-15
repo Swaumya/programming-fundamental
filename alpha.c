02217b8989cb076de504ba3bec181c78ba3d06ad
Swaumya <pokharelswaumya@gmail.com>
2017-01-15 10:48:29 +0545

    Add files via upload

---
+++ alpha.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+	int main()
+             {
+                int i;
+                    char ch='A';
+                    for(i=1;i<=26;i++)
+                    {
+                      printf("%c\t",ch);
+                          ch++;
+                         }
+                        return 0;
+                      }
