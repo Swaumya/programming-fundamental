1aa8d3f3d2e3f287f88e181014d19750d2eba211
Swaumya <pokharelswaumya@gmail.com>
2017-01-17 10:29:46 +0545

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+ #include<stdio.h>
+  #include<stdlib.h>
+     void cube ();
+     int main()
+
+       {
+          cube();
+          return 0;
+         }
+        void cube()
+        {
+           
+           printf("hello world :\n");
+          
+     
+     }
