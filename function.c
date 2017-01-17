3c7c2bd3a3a7f2bf434a677086123e32ede8201d
Swaumya <pokharelswaumya@gmail.com>
2017-01-17 10:29:31 +0545

    Add files via upload

---
+++ function.c
@@ -0,0 +1,18 @@
+   #include<stdio.h>
+     void cube ();
+     int main()
+
+       {
+          cube();
+          return 0;
+         }
+        void cube()
+        {
+           int a,cube;
+           printf("Enter any number :\n");
+           scanf("%d",&a);
+           cube=a*a*a;
+           printf(" The cube of number is %d,",cube);
+      
+     }
+
